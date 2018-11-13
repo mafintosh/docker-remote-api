88270282f6b36643147d3501acd819ffc472736b
Steven <steven@ceriously.com>
2018-11-13 15:03:16 -0500

    Fix whitespace

--- types.d.ts
+++ types.d.ts
@@ -4,13 +4,13 @@ declare module 'docker-remote-api' {
     }
 
     interface GetOptions {
-        qs?: {[name:string]: string},// set querystring parameters
-        headers?: {[name:string]: string},// set request headers
-        json?: boolean,             // return json instead of a stream
-        buffer?: boolean,           // return a buffer instead of a stream
-        drain?: boolean,            // will drain the response stream before calling cb
-        timeout?: number,         // set request timeout
-        version: string        // set explicit api version "v1.14" (required)
+        qs?: { [name: string]: string };      // set querystring parameters
+        headers?: { [name: string]: string }; // set request headers
+        json?: boolean;                      // return json instead of a stream
+        buffer?: boolean;                    // return a buffer instead of a stream
+        drain?: boolean;                     // will drain the response stream before calling cb
+        timeout?: number;                    // set request timeout
+        version: string;                     // set explicit api version "v1.14" (required)
     }
 
     interface PostOptions extends GetOptions {
@@ -22,7 +22,7 @@ declare module 'docker-remote-api' {
         delete<T>(path: string, opt: GetOptions, fn: (err: Error, data: T) => void): void;
         head<T>(path: string, opt: GetOptions, fn: (err: Error, data: T) => void): void;
         post<T>(path: string, opt: PostOptions, fn: (err: Error, data: T) => void): void;
-		put<T>(path: string, opt: PostOptions, fn: (err: Error, data: T) => void): void;
+        put<T>(path: string, opt: PostOptions, fn: (err: Error, data: T) => void): void;
     }
 
     function DockerInit(opt: InitOptions): DockerRequest;
